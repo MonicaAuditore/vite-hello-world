790869bedac3702ff7a67a4448978d35e8faf975
MonicaAuditore <mauditore@live.it>
2023-02-01 14:43:53 +0100

    add_nuovo componente

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import AppTitle from "./components/AppTitle.vue";
+export default {
+  components: {
+    AppTitle,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<template>
+  <AppTitle />
+  <!-- <button @click="count++">Count is: {{ count }}</button> -->
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      message: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
